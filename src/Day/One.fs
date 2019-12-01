2431e12fc70a458ba4fbe4965b9d4d4ef15aa512
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2019-12-01 15:16:08 -0500

    Add Day One Part Two

--- src/Day/One.fs
+++ src/Day/One.fs
@@ -1,9 +1,26 @@
 module Day.One
+
 open System
 open System.IO
-// open System.Math
 
-(*
+let kestrel x y =
+    y x
+    x
+
+let dayPath = __SOURCE_DIRECTORY__
+
+let input =
+    Path.Combine [| dayPath; "ModuleMasses.txt" |]
+    |> File.ReadAllLines
+    |> Seq.map Convert.ToDouble
+
+let moduleFuel mass =
+    mass / 3.0
+    |> floor
+    |> fun m -> m - 2.0
+
+module PartOne =
+    (*
 --- Day 1: The Tyranny of the Rocket Equation ---
 Santa has become stranded at the edge of the Solar System while delivering presents to other planets! To accurately
 calculate his position in space, safely align his warp drive, and return to Earth in time to save Christmas, he needs
@@ -32,26 +49,46 @@ the mass of each module (your puzzle input), then add together all the fuel valu
 What is the sum of the fuel requirements for all of the modules on your spacecraft?
 *)
 
-(*
+    (*
   G Elves = {e| e is an elf, and e is Go until Fuel Counter-Upper}
   U SumOfFuelRequirementsForAllModules = ?
   E ModuleFuel = Floor(mass / 3) - 2
 *)
-let dayPath = __SOURCE_DIRECTORY__
+    let output = Seq.sumBy moduleFuel input
 
-let moduleFuel mass =
-    mass / 3.0
-    |> Math.Floor
-    |> fun m -> m - 2.0
+module PartTwo =
+    (*
+--- Part Two ---
+During the second Go / No Go poll, the Elf in charge of the Rocket Equation Double-Checker stops the launch sequence.
+Apparently, you forgot to include additional fuel for the fuel you just added.
 
-let a x y = 
-    y x
-    x
+  Fuel itself requires fuel just like a module - take its mass, divide by three, round down, and subtract 2. However,
+that fuel also requires fuel, and that fuel requires fuel, and so on. Any mass that would require negative fuel should
+instead be treated as if it requires zero fuel; the remaining mass, if any, is instead handled by wishing really hard,
+which has no mass and is outside the scope of this calculation.
 
-let input = 
-    Path.Combine [| dayPath; "ModuleMasses.txt" |]
-    |> File.ReadAllLines
-    |> a <| printfn "%A"
-    |> Seq.map Convert.ToDouble
-    
-let output = Seq.sumBy moduleFuel input
+  So, for each module mass, calculate its fuel and add it to the total. Then, treat the fuel amount you just calculated
+as the input mass and repeat the process, continuing until a fuel requirement is zero or negative. For example:
+
+  A module of mass 14 requires 2 fuel. This fuel requires no further fuel (2 divided by 3 and rounded down is 0, which
+would call for a negative fuel), so the total fuel required is still just 2.
+
+  At first, a module of mass 1969 requires 654 fuel. Then, this fuel requires 216 more fuel (654 / 3 - 2). 216 then
+requires 70 more fuel, which requires 21 fuel, which requires 5 fuel, which requires no further fuel. So, the total
+fuel required for a module of mass 1969 is 654 + 216 + 70 + 21 + 5 = 966.
+
+  The fuel required by a module of mass 100756 and its fuel is:
+33583 + 11192 + 3728 + 1240 + 411 + 135 + 43 + 12 + 2 = 50346.
+
+  What is the sum of the fuel requirements for all of the modules on your spacecraft when also taking into account the
+  mass of the added fuel? (Calculate the fuel requirements for each module separately,
+  then add them all up at the end.)
+*)
+    let rec calc mass =
+        let tot = moduleFuel mass
+
+        match tot with
+        | tot when tot > 0.0 -> tot + calc tot
+        | _ -> 0.0
+
+    let output = Seq.sumBy calc input
--- src/Day/Program.fs
+++ src/Day/Program.fs
@@ -1,8 +1,23 @@
 ﻿module Day.Program
 open System
 
+let line =
+    String.replicate 80 "-"
+
 [<EntryPoint>]
 let main argv =
-    printfn "Day One:"
-    printfn "%f" Day.One.output
+    printfn "%s" line
+    printfn "# Day One:"
+    printfn "%s" line
+    printfn "## Input:"
+    printfn "%A" <| Seq.toList Day.One.input
+    printfn "%s" line
+    printfn "## Part One:"
+    // 3348430
+    printfn "Output: %f" Day.One.PartOne.output
+    printfn "%s" line
+    printfn "Part Two:"
+    // 5019767
+    printfn "Output: %f" Day.One.PartTwo.output
+    printfn "%s" line
     0
